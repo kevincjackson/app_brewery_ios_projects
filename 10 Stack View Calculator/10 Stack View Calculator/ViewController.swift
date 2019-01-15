123b9cb21bb70ab9caf1094fddd9218d070f49e4
Kevin Jackson <kj31428@gmail.com>
2019-01-15 12:05:14 -0800

    Adds Practice Layout with stack views.

---
+++ 10 Stack View Calculator/10 Stack View Calculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  10 Stack View Calculator
+//
+//  Created by Kevin Jackson on 1/15/19.
+//  Copyright © 2019 Kevin Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
+}
+
